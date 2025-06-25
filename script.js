3300557d818cad53b5c377354555d24024879c0b
Dev-id-A <davidaranzana618@gmail.com>
2025-06-25 19:24:35 +0200

    Treemap displayed

--- script.js
+++ script.js
@@ -17,5 +17,20 @@
 
     d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json")
         .then(gamesData => {
-            console.log(gamesData)
+//Treemap creation
+            const root = d3.hierarchy(gamesData).sum(d => d.value);
+
+            d3.treemap()
+                .size([width,height])
+                .padding(1)(root);
+
+            svg.selectAll("rect")
+                .data(root.leaves())
+                .join("rect")
+                .attr("y", d => d.y0)
+                .attr("x", d => d.x0)
+                .attr("width", d => d.x1 - d.x0)
+                .attr("height", d => d.y1 - d.y0)
+                .style("fill", "red");
+
         })
