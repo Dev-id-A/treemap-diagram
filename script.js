afaa01984a80c57f6c0808942f37ab996b89e696
Dev-id-A <davidaranzana618@gmail.com>
2025-06-26 18:12:30 +0200

    Text displayed

--- script.js
+++ script.js
@@ -18,19 +18,39 @@
     d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json")
         .then(gamesData => {
 //Treemap creation
-            const root = d3.hierarchy(gamesData).sum(d => d.value);
+            const gamesRoot = d3.hierarchy(gamesData).sum(d => d.value);
 
             d3.treemap()
                 .size([width,height])
-                .padding(1)(root);
+                .padding(1)(gamesRoot);
 
             svg.selectAll("rect")
-                .data(root.leaves())
+                .data(gamesRoot.leaves())
                 .join("rect")
+                .attr("class", "tile")
                 .attr("y", d => d.y0)
                 .attr("x", d => d.x0)
                 .attr("width", d => d.x1 - d.x0)
                 .attr("height", d => d.y1 - d.y0)
                 .style("fill", "red");
 
+//Text addition
+svg.selectAll("text")
+    .data(gamesRoot.leaves())
+    .join("text")
+    .attr("y", d => d.y0 + 10)
+    .style("font-size", "9px")
+    .each(function(d){
+        const word = d.data.name.split(" ");
+
+        const displayedWords = word.length >= 3 ? word.slice(0,3).concat("..."): word
+
+        d3.select(this)
+            .selectAll("tspan")
+            .data(displayedWords)
+            .join("tspan")
+            .attr("x",d.x0 + 1)
+            .attr("dy", (d, i) => i === 0 ? 0: "1.2em")
+            .text(d => d);
+    });
         })
